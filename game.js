88388aa3583101503d167b4d9c016ba8b652713e
ariahsargent <sailorsargent@live.com>
2021-04-04 04:13:57 -0600

    still needs work- working on it

--- game.js
+++ game.js
@@ -13,9 +13,22 @@
 //game logic
 var playerHealth = 50;
 var enemyHealth = 50;
-var gameInfo = "Play By Clicking Buttons";
+var gameInfo = "Discover the Secret by Pressing Play";
 var cardsBankLength = 100;
-var playerCards = [];
+
+var playerCards = [
+    // [image, cardName, health, attack, defense, dead?]
+    //insible woman
+    [src="Pictures\InvisibleWoman.jpg",
+    "Invisible Woman", 100, 4, 10, false],
+    //human torch
+    [src="Pictures\TheTorch.jpg",
+    "The Human Torch", 100, 9, 7, false],
+    //mr fantastic
+    [src="Pictures\MrFantastic.jpg", 100, 7, 6, false],
+    //the thing
+    [src="Pictures\TheThing.jpg", 100, 9, 9, false]
+];
 var enemyCards = [];
 
 var cardsDamage; 
@@ -24,6 +37,7 @@ var cardsName;
 var cards;
 var cardImage;
 
+var gameOver = false;
 var playerDone = false;
 var initialized = false;
 var lastCardID = 0;
@@ -31,45 +45,281 @@ var enemyAIInterval;
 var highlightChoice = 4;
 
 
+
+
 //display variables
 var gameInfoDisp = document.getElementById('info');
 var playerHealthDisp = document.getElementById('playerHealth');
 var enemyHealthDisp = document.getElementById('enemyHealth');
 
-//event variables
+enableAllButtons();
 const playerTurnButton = document.getElementById('playerTurn');
 playerTurnButton.addEventListener('click', playerAttack);
-console.log("player attacked");
 
 const doneButton = document.getElementById('done');
 doneButton.addEventListener('click', done);
-console.log("done button clicked");
 
 const upgradeButton = document.getElementById('play');
 upgradeButton.addEventListener('click', upgrade);
-console.log("upgrade button clicked");
 
-console.log(gameInfoDisp);
-console.log(playerHealthDisp);
-console.log(enemyHealthDisp);
+playerTurnButton.disabled = false;
+doneButton.disabled = false;  
+upgradeButton.disabled = false;
 
+function ididit() {
+    console.log("i did it");
+}
 
 function playerAttack() {
-    //if(!gameOver()) {
-    console.log("In player attack");
-    //cardBattle();
-    //gameOver();
-    //playerDone = true;
-    //disableAllButDone();
-    //}
+    /*if(!gameOver()) {
+        console.log("In player attack");
+        cardBattle();
+        gameOver();
+        playerDone = true;
+        disableAllButDone();
+    }*/
+    console.log("playerAttack");
 }
 
 function playerTurn(){
-    //if(!gameOver() && !playerDone) {
-    enableAllButtons();
-    //gameOver();
+    if(!gameOver() && !playerDone) {
+        enableAllButtons();
+        //gameOver();
+    }
+    console.log("players turn");
+}
+
+function setUpCardBanks() {
+    for (var i = 0; i < cardsBankLength; i++) {
+        console.log(i);
+        /*
+        var cardStats = getRandomStats();
+        // [1, 2, 3, 4]
+        cardStats.push(getRandomName());
+        // [1, 2, 'random name']   -  for enemy cards
+        cardStats.push(getRandomFanasticFour());
+        //get random fantastic four hero for player
+        cardStats.push(getRandomColor());
+        // [1, 2, 'random name', 'random color']
+        cardStats.push(i);
+        // [1, 2, 'random name', 'random color', 'enemy_id_3' 'image url', dead? 0 is dead, 1 is alive]
+
+        //our image is in position 5
+        cardStats.push(getRandomImageURL());
+
+        //this will tell us if the card is in play at position 6:
+        
+        // the first 3 enemy cards and first 3 player cards are in play (index 0,1,2 and 50, 51, 52)
+        if (i < 3) {
+            cardStats.push(1);
+            console.log('enemy card in play index is: ' + i);
+
+        }
+        else if (i >= cardsBankLength/2 && i < (cardsBankLength/2 + 3)) {
+            cardStats.push(1);
+            console.log('player card in play index is: ' + i);
+        }
+        else {
+            cardStats.push(0);
+        }
+        //console.log("card stats: " + cardStats);
+        // [3, 6, 'aoughalkh'];
+        if (i < cardsBankLength/2 ) {
+            playerCards.push(cardStats);
+        }
+        else {
+            enemyCards.push(cardStats);
+            // [[1, 2, 'name'],[1, 2, 'other name']]
+        }*/
+    }
+}
+
+function initializeCards(){
+    //console.log("Calling");
+    //Get a list of all the cards damage in play in the game:
+    
+    cardsHealth = document.getElementsByClassName('card-health'); //added
+    cardsDamage = document.getElementsByClassName('card-attack');
+    cardsDefense= document.getElementsByClassName('card-defense');
+    cardsName = document.getElementsByClassName('card-name');
+    cards = document.getElementsByClassName('card');
+    cardImage = document.getElementsByTagName('img');
+    //for player card intializations:
+    for (var i = 0; i < cardsBankLength; i++) {
+
+    /*
+    //enemy card initializiations:
+    for (var i = 0; i < 6; i++) {
+        //need to run a check to see if the card is already dead:
+        if (!cardIsDead(i)) {
+
+        //enemy cards: 
+        if(i < 3) {
+        //set the attack of the enemy card damage by accessing the list and assignting it the value of enemycards at first array first index
+        //[[2, 3, 'aadfuaio' ], [2, 3, 'aadfuaio' ], [2, 3, 'aadfuaio' ]]
+        cardsDamage[i].innerHTML = "Attack: " + enemyCards[i][0];
+        //set the defense
+        cardsDefense[i].innerHTML = "Defense: " + enemyCards[i][1];
+        //set the name
+        cardsName[i].innerHTML = enemyCards[i][2];
+
+        cards[i].style.backgroundColor = enemyCards[i][3];
+        cardImage[i].src = enemyCards[i][5];
+        cards[i].id = 'enemyCard' + i;
+
+        }
+        //player cards:
+        else {
+        //set the attack of the enemy card damage by accessing the list at i
+        cardsDamage[i].innerHTML = "Attack: " + playerCards[i-3][0];
+        //set the defense
+        cardsDefense[i].innerHTML = "Defense: " +  playerCards[i-3][1];
+        //set the name
+        cardsName[i].innerHTML = playerCards[i-3][2];
+        cards[i].style.backgroundColor = playerCards[i-3][3];
+        cardImage[i].src = playerCards[i-3][5];
+        cards[i].id = 'playerCard' + i;
+        if (!initialized) {
+            cards[i].addEventListener('click',selectedCard, false);
+        }
+
+        }
+    }
+    else if (cardIsDead(i)){
+        cards[i].style.backgroundColor = "black";
+        console.log("deteced death of: " + cards[i].id);
+    }
+}
+    */
+     console.log("initializeCards function");
+
+    
+}
+
+function enemyTurn() {
+    /*if(!gameOver()) {
+        disableAllButtons();
+        simulateThinking();
+        setTimeout(enemyChoice, 3200);
+        gameOver();
+    }*/
+    disableAllButtons();
+    simulateThinking();
+    setTimeout(enemyChoice, 3200);
+    console.log("enemys turn");
+}
+
+function simulateThinking() {
+    console.log("simulatedThinking function");
+    //enemyAIInterval = setInterval(enemyAI, 500);
+    //essentially this makes it player turn again:
+    setTimeout(enableAllButtons, 3000);
+}
+function enemyAI() {
+    //first 3 cards are enemy cards
+    console.log("enemyAI function");
+    //highlightChoice = parseInt(Math.random() * 3);
+    //cards[highlightChoice].style.backgroundColor = cards[highlightChoice].style.backgroundColor == "red" ? enemyCards[i][3] : "red";
+    //cards[highlightChoice].style.backgroundColor = "#c00000"
+}
+
+function enableAllButtons() {
+    //if (initialized) {
+    //    clearInterval(enemyAIInterval);
     //}
-} /*
+    //initialized = true;
+    //initializeCards();
+   // playerTurnButton.disabled = false;
+    //doneButton.disabled = false;  
+    //upgradeButton.disabled = false;
+    console.log("enableAllButtons function");
+
+}
+
+function done() {
+    playerDone = true;
+    console.log("done function");
+    disableAllButtons();
+    enemyTurn();
+}
+
+function disableAllButDone(){
+    playerTurnButton.disabled = false;
+    doneButton.disabled = false;  
+    upgradeButton.disabled = true;
+    console.log("disableAllButDone function");
+}
+
+function disableAllButtons() {
+    playerTurnButton.disabled = true;
+    doneButton.disabled = true;  
+    upgradeButton.disabled = true;
+    console.log("disableAllButtons function");
+}
+
+function upgrade() {
+    //console.log("Upgrade: " + lastCardID);
+    //playerCards[lastCardID][0] += (parseInt(Math.random() * playerCards[lastCardID][0] + playerCards[lastCardID][0]/4));
+    //playerCards[lastCardID][1] += (parseInt(Math.random() * playerCards[lastCardID][1] + playerCards[lastCardID][1]/4));
+    
+    console.log("upgrade function");
+    initializeCards();
+    disableAllButDone();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//event variables
+
+console.log(gameInfoDisp);
+console.log(playerHealthDisp);
+console.log(enemyHealthDisp); */
+
+/*
+function playerAttack() {
+    if(!gameOver()) {
+    console.log("In player attack");
+    cardBattle();
+    gameOver();
+    playerDone = true;
+    disableAllButDone();
+    }
+}
+
+
 function setUpCardBanks() {
     for (var i = 0; i < cardsBankLength; i++) {
 
@@ -286,11 +536,6 @@ function initializeCards(){
     }
 }
 
-function disableAllButDone(){
-    playerTurnButton.disabled = true;
-    doneButton.disabled = false;  
-    upgradeButton.disabled = true;
-}
 
 function selectedCard(e) {
     console.log(this);
@@ -321,14 +566,7 @@ function getRandomImageURL() {
     return ret;
 }
 
-function enemyTurn() {
-    if(!gameOver()) {
-    disableAllButtons();
-    simulateThinking();
-    setTimeout(enemyChoice, 3200);
-    gameOver();
-    }
-}
+
 function enemyChoice() {
     //find a card that is alive:
     for (i = 0; i < 3; i++) {
@@ -376,18 +614,18 @@ function startGame() {
     setUpCardBanks();
     initializeCards();
     enableAllButtons();
-}*/
+}
 function enableAllButtons() {
-    //if (initialized) {
-    //    clearInterval(enemyAIInterval);
-    //}
-    //initialized = true;
-    //initializeCards();
+    if (initialized) {
+        clearInterval(enemyAIInterval);
+    }
+    initialized = true;
+    initializeCards();
     playerTurnButton.disabled = false;
     doneButton.disabled = false;  
     upgradeButton.disabled = false;
 
-}/*
+}
 function upgrade() {
     console.log("Upgrade: " + lastCardID);
     playerCards[lastCardID][0] += (parseInt(Math.random() * playerCards[lastCardID][0] + playerCards[lastCardID][0]/4));
@@ -451,4 +689,5 @@ function gameOver() {
     return false;
 } */
 
-startGame(); 
+//startGame(); 
+}
